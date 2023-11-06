fd2e13de4a01cf31ffedb17ad95279da6e447937
Its-Yayo <a01754574@itesm.mx>
2023-11-06 10:33:27 -0600

    calculator.rs: add resources

---
+++ src/calculator.rs
@@ -0,0 +1,14 @@
+macro_rules! calculator {
+    ($operator: ident, $num1: expr, $num2: expr) => {
+        match $operator {
+            "add" => $num1 + $num2,
+            "multiply" => $num1 * $num2,
+            "subtract" => $num1 - $num2,
+            "divide" => $num1 / $num2,
+        }
+    };
+}
+
+fn call_this(method: &str, int_1: i32, int_2: i32) {
+    calculator!(method, int_1, int_2);
+}
