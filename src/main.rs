eb4c634c5e45883c8cf1d01ad2b0b70803e71922
Its-Yayo <a01754574@itesm.mx>
2024-01-04 16:46:34 -0600

    numbers added init

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,15 @@ fn main() {
     io::stdin().read_line(&mut operator).expect("Failed to read line");
     let operator = operator.trim();
 
+    let mut num1 = String::new();
+    println!("First number!: ");
+    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    let num1: f64 = num1.trim().parse().expect("Invalid number for num1");
+
+    let mut num1 = String::new();
+    println!("First number!: ");
+    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    let num1: f64 = num1.trim().parse().expect("Invalid number for num1");
+
 
 }
