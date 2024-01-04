13e7d3d2dbb6ca2a4a90fd16827d5d8f638c22a2
Its-Yayo <a01754574@itesm.mx>
2024-01-04 16:47:27 -0600

    numbers added init

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,12 @@ fn main() {
     io::stdin().read_line(&mut num1).expect("Failed to read line");
     let num1: f64 = num1.trim().parse().expect("Invalid number for num1");
 
-    let mut num1 = String::new();
+    let mut num2 = String::new();
     println!("First number!: ");
-    io::stdin().read_line(&mut num1).expect("Failed to read line");
-    let num1: f64 = num1.trim().parse().expect("Invalid number for num1");
+    io::stdin().read_line(&mut num2).expect("Failed to read line");
+    let num2: f64 = num2.trim().parse().expect("Invalid number for num2");
+
+    // Here goes the macro...
 
 
 }
