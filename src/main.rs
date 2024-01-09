0fc38340e1c47dd4f83df6ff509ac9d55d0660e7
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-01-09 15:09:21 -0600

    comment io

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod calculator;
 use std::io;
 
 fn main() {
-    println!("Macro Calc: Single Calculator macro");
+    println!("Macro Calc - Single Calculator macro");
 
     let mut operator = String::new();
     println!("Type the operator [add/multiply/subtract/divide]: ");
